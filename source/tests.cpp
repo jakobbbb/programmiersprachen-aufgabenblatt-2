0e4014ae284cbf5ebaa86cda9e4f30b8691a7fa2
Jakob Ruckel <jakob@ruckel.de>
2020-05-13 17:48:26 +0200

    add vec2 (2.2)

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,15 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+TEST_CASE("describe_vec2", "[vec2]") {
+  Vec2 a;
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f);
+  Vec2 b{4.2f, -9.3f};
+  REQUIRE(b.x == Approx(4.2f));
+  REQUIRE(b.y == Approx(-9.3f));
+}
 
 int main(int argc, char *argv[])
 {
