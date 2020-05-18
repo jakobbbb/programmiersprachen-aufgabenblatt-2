f34dd47c6a78b01fdb51a2193ec4b3428ef34eb8
Jakob Ruckel <jakob@ruckel.de>
2020-05-18 17:12:23 +0200

    mat2: add det, invert, transpose, rotation (2.6)

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,4 +1,5 @@
 #include "mat2.hpp"
+#include <cmath>
 
 Mat2& Mat2::operator*=(Mat2 const& m) {
   Mat2 r = *this * m;
@@ -16,4 +17,33 @@ Mat2 operator*(Mat2 const& a, Mat2 const& b) {
   r.e_01 = a.e_01 * b.e_00 + a.e_11 * b.e_01;
   r.e_11 = a.e_01 * b.e_10 + a.e_11 * b.e_11;
   return r;
+}
+
+float Mat2::det() const {
+  return e_00 * e_11 - e_10 * e_01;
+}
+
+Mat2 operator*(Mat2 const& m, float s) {
+  Mat2 r{m};
+  r.e_00 *= s;
+  r.e_01 *= s;
+  r.e_10 *= s;
+  r.e_11 *= s;
+  return r;
+}
+
+Mat2 inverse(Mat2 const& m) {
+  float d = 1 / m.det();
+  return Mat2{d * m.e_11, d * -m.e_10,
+              d * -m.e_01, d * m.e_00};
+}
+
+Mat2 transpose(Mat2 const& m) {
+  return Mat2{m.e_00, m.e_01,
+              m.e_10, m.e_11};
+}
+
+Mat2 rotation_matrix(float phi) {
+  return Mat2{std::cos(phi), -std::sin(phi),
+              std::sin(phi), std::cos(phi)};
 }
--- source/tests.cpp
+++ source/tests.cpp
@@ -153,4 +153,75 @@ TEST_CASE("mat2 *", "[mat2]") {
   REQUIRE(d.e_11 == Approx(-30.38f));
 }
 
+TEST_CASE("mat2 det", "[mat2]") {
+  const Mat2 a{-1.3f, 5.2f, 0.3f, 0.0f};
+  const Mat2 b{4.2f, -9.3f, 5.2f, -6.2f};
+  const Mat2 i;
+  REQUIRE(a.det() == Approx(-1.56f));
+  REQUIRE(b.det() == Approx(22.32));
+  REQUIRE(i.det() == 1);
+}
+
+TEST_CASE("mat2 inverse", "[mat2]") {
+  const Mat2 a{-1.3f, 5.2f, 0.3f, 0.0f};
+  Mat2 a_inv = inverse(a);
+  REQUIRE(a_inv.e_00 == Approx(0));
+  REQUIRE(a_inv.e_01 == Approx(0.192308));
+  REQUIRE(a_inv.e_10 == Approx(3.33333));
+  REQUIRE(a_inv.e_11 == Approx(0.833333));
+
+  const Mat2 b{4.2f, -9.3f, 5.2f, -6.2f};
+  Mat2 b_inv = inverse(b);
+  REQUIRE(b_inv.e_00 == Approx(-0.277778));
+  REQUIRE(b_inv.e_01 == Approx(-0.232975));
+  REQUIRE(b_inv.e_10 == Approx(0.416667));
+  REQUIRE(b_inv.e_11 == Approx(0.188172));
+
+  const Mat2 i;
+  Mat2 i_inv = inverse(i);
+  REQUIRE(i_inv.e_00 == 1);
+  REQUIRE(i_inv.e_01 == 0);
+  REQUIRE(i_inv.e_10 == 0);
+  REQUIRE(i_inv.e_11 == 1);
+}
+
+TEST_CASE("mat2 transpose", "[mat2]") {
+  const Mat2 a{-1.3f, 5.2f, 0.3f, 0.0f};
+  Mat2 a_t = transpose(a);
+  REQUIRE(a_t.e_00 == a.e_00);
+  REQUIRE(a_t.e_01 == a.e_10);
+  REQUIRE(a_t.e_10 == a.e_01);
+  REQUIRE(a_t.e_11 == a.e_11);
+
+  const Mat2 i;
+  Mat2 i_t = transpose(i);
+  REQUIRE(i_t.e_00 == 1);
+  REQUIRE(i_t.e_01 == 0);
+  REQUIRE(i_t.e_10 == 0);
+  REQUIRE(i_t.e_11 == 1);
+}
+TEST_CASE("mat2 rotation matrix", "[mat2]") {
+  // Mat2 rotation_matrix(float phi);
+  const float alpha = 0;
+  Mat2 alpha_rot = rotation_matrix(alpha);
+  REQUIRE(alpha_rot.e_00 == 1);
+  REQUIRE(alpha_rot.e_01 == 0);
+  REQUIRE(alpha_rot.e_10 == 0);
+  REQUIRE(alpha_rot.e_11 == 1);
+
+  const float beta = 4.2f;
+  Mat2 beta_rot = rotation_matrix(beta);
+  REQUIRE(beta_rot.e_00 == Approx(-0.490261));
+  REQUIRE(beta_rot.e_01 == Approx(-0.871576));
+  REQUIRE(beta_rot.e_10 == Approx(0.871576));
+  REQUIRE(beta_rot.e_11 == Approx(-0.490261));
+
+  const float gamma = 2 * M_PI;
+  Mat2 gamma_rot = rotation_matrix(gamma);
+  REQUIRE(gamma_rot.e_00 == Approx(1.0f));
+  REQUIRE(gamma_rot.e_01 == Approx(0.0f));
+  REQUIRE(gamma_rot.e_10 == Approx(0.0f));
+  REQUIRE(gamma_rot.e_11 == Approx(1.0f));
+}
+
 int main(int argc, char *argv[]) { return Catch::Session().run(argc, argv); }
